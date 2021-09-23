c3f8880e6081523d4e3aa9eaa7a51c7dc4141ae0
Corbeno <corbin@kemsfamily.us>
2021-09-22 21:09:41 -0500

    initial spring create

---
+++ Backend/scribbleshare/src/main/java/edu/iastate/scribbleshare/ScribbleshareApplication.java
@@ -0,0 +1,13 @@
+package edu.iastate.scribbleshare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScribbleshareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScribbleshareApplication.class, args);
+	}
+
+}
