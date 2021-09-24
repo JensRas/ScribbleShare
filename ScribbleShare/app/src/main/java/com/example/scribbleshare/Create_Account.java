2b1d89dfc1494c8d5865b5d935e480ce5a2913b6
Blake <blakefisher24@gmail.com>
2021-09-24 11:45:34 -0500

    Made good progress on Create Account page

---
+++ ScribbleShare/app/src/main/java/com/example/scribbleshare/Create_Account.java
@@ -0,0 +1,17 @@
+package com.example.scribbleshare;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class Create_Account extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_account);
+    }
+}
--- ScribbleShare/app/src/main/java/com/example/scribbleshare/MainActivity.java
+++ ScribbleShare/app/src/main/java/com/example/scribbleshare/MainActivity.java
@@ -1,14 +1,27 @@
 package com.example.scribbleshare;
 
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+    Button create_account;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        create_account=(Button) findViewById(R.id.button);
+        create_account.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(view.getContext(), Create_Account.class));
+            }
+        });
     }
 }
