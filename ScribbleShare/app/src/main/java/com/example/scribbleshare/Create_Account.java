1bf149adc742c8c0f912a0ee302652c5da3f3fc9
Blake <blakefisher24@gmail.com>
2021-09-27 10:47:39 -0500

    Started Sign In

--- ScribbleShare/app/src/main/java/com/example/scribbleshare/Create_Account.java
+++ ScribbleShare/app/src/main/java/com/example/scribbleshare/Create_Account.java
@@ -4,7 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.method.HideReturnsTransformationMethod;
+import android.text.method.PasswordTransformationMethod;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 public class Create_Account extends AppCompatActivity {
 
@@ -13,5 +19,6 @@ public class Create_Account extends AppCompatActivity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account);
+
     }
 }
