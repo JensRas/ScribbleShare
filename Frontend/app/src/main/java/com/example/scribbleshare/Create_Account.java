a66f80ac87146fc2b85232f5ffed73463b5a4d21
Corbeno <corbin@kemsfamily.us>
2021-09-29 21:52:29 -0500

    initial commit. Not working yet

--- Frontend/app/src/main/java/com/example/scribbleshare/Create_Account.java
+++ Frontend/app/src/main/java/com/example/scribbleshare/Create_Account.java
@@ -6,12 +6,23 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.text.method.HideReturnsTransformationMethod;
 import android.text.method.PasswordTransformationMethod;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class Create_Account extends AppCompatActivity {
 
@@ -34,6 +45,49 @@ public class Create_Account extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(view.getContext(), MainActivity.class));
+            }
+        });
+
+        Button sign_in_button = (Button) findViewById(R.id.sign_in_button);
+        sign_in_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                RequestQueue queue = Volley.newRequestQueue(view.getContext());
+                Log.d("clicked", "CLICKED BUTTON");
+                String url = "http://localhost:8080/users/new";
+                StringRequest putRequest = new StringRequest(Request.Method.PUT, url,
+                        new Response.Listener<String>()
+                        {
+                            @Override
+                            public void onResponse(String response) {
+                                // response
+                                Log.d("Response", response);
+                            }
+                        },
+                        new Response.ErrorListener()
+                        {
+                            @Override
+                            public void onErrorResponse(VolleyError error) {
+                                // error
+                                Log.d("Error.Response", error.toString());
+                            }
+                        }
+                ) {
+
+                    @Override
+                    protected Map<String, String> getParams()
+                    {
+                        Map<String, String>  params = new HashMap<String, String>();
+                        params.put("username", "ANDROIDUSER");
+                        params.put("password", "password");
+
+                        return params;
+                    }
+
+                };
+
+                queue.add(putRequest);
+                queue.start();
             }
         });
     }
