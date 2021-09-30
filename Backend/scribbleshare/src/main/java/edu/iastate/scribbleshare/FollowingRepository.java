999b1f8d5d31a3fcf5d038c3d0e337596508e1b1
Cole Langner <clangner@iastate.edu>
2021-09-29 21:05:22 -0500

    added starting endpoints for user/following table

---
+++ Backend/scribbleshare/src/main/java/edu/iastate/scribbleshare/FollowingRepository.java
@@ -0,0 +1,19 @@
+package edu.iastate.scribbleshare;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+public interface FollowingRepository extends CrudRepository<Follower, String>{
+    
+    Follower findByUsername(String username);
+
+
+   @Query(value="select * from follower u where u.username=:username", nativeQuery = true)
+   public Iterable<Follower> queryUsers(@Param("username") String username);
+
+   @Query(value="select * from follower u where u.following=:following", nativeQuery = true)
+   public Iterable<Follower> queryFollowers(@Param("following") String following);
+
+
+}
