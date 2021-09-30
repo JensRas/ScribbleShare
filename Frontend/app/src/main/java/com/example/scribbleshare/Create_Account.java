64fd8f86f18919c6412aeb040da721d645d08714
Corbeno <corbin@kemsfamily.us>
2021-09-29 23:12:41 -0500

    tried more with volley.. couldn't get it to work still

--- Frontend/app/src/main/java/com/example/scribbleshare/Create_Account.java
+++ Frontend/app/src/main/java/com/example/scribbleshare/Create_Account.java
@@ -1,5 +1,7 @@
 package com.example.scribbleshare;
 
+import static android.content.ContentValues.TAG;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -13,6 +15,7 @@ import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -25,6 +28,10 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Create_Account extends AppCompatActivity {
+
+    private RequestQueue mRequestQueue;
+    private StringRequest mStringRequest;
+    private String url = "http://www.mocky.io/v2/597c41390f0000d002f4dbd1";
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -52,43 +59,71 @@ public class Create_Account extends AppCompatActivity {
         sign_in_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                RequestQueue queue = Volley.newRequestQueue(view.getContext());
-                Log.d("clicked", "CLICKED BUTTON");
-                String url = "http://localhost:8080/users/new";
-                StringRequest putRequest = new StringRequest(Request.Method.PUT, url,
-                        new Response.Listener<String>()
-                        {
-                            @Override
-                            public void onResponse(String response) {
-                                // response
-                                Log.d("Response", response);
-                            }
-                        },
-                        new Response.ErrorListener()
-                        {
-                            @Override
-                            public void onErrorResponse(VolleyError error) {
-                                // error
-                                Log.d("Error.Response", error.toString());
-                            }
-                        }
-                ) {
-
-                    @Override
-                    protected Map<String, String> getParams()
-                    {
-                        Map<String, String>  params = new HashMap<String, String>();
-                        params.put("username", "ANDROIDUSER");
-                        params.put("password", "password");
-
-                        return params;
-                    }
-
-                };
-
-                queue.add(putRequest);
-                queue.start();
+                testRequest();
+//                RequestQueue queue = Volley.newRequestQueue(view.getContext());
+//                Log.d("clicked", "CLICKED BUTTON");
+//                String url = "http://localhost:8080/users/new";
+//                StringRequest putRequest = new StringRequest(Request.Method.PUT, url,
+//                        new Response.Listener<String>()
+//                        {
+//                            @Override
+//                            public void onResponse(String response) {
+//                                // response
+//                                Log.d("Response", response);
+//                            }
+//                        },
+//                        new Response.ErrorListener()
+//                        {
+//                            @Override
+//                            public void onErrorResponse(VolleyError error) {
+//                                // error
+//                                Log.d("Error.Response", error.toString());
+//                            }
+//                        }
+//                ) {
+//
+//                    @Override
+//                    protected Map<String, String> getParams()
+//                    {
+//                        Map<String, String>  params = new HashMap<String, String>();
+//                        params.put("username", "ANDROIDUSER");
+//                        params.put("password", "password");
+//
+//                        return params;
+//                    }
+//
+//                };
+//
+//                queue.add(putRequest);
+//                queue.start();
             }
         });
+    }
+
+    private void testRequest(){
+        Log.d("test", "started test request");
+        //RequestQueue initialized
+        mRequestQueue = Volley.newRequestQueue(this);
+
+        //String Request initialized
+        mStringRequest = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+
+                Toast.makeText(getApplicationContext(),"Response :" + response.toString(), Toast.LENGTH_LONG).show();//display the response on screen
+                Log.d("response", response.toString());
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+                Log.i(TAG,"Error :" + error.toString());
+            }
+        });
+
+        mRequestQueue.add(mStringRequest);
+
+        Log.d("test", "finished test request");
+
     }
 }
