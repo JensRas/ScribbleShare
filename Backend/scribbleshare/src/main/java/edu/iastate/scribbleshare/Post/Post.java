c699e6b16723c7b6aa9905289d2db860e7871b25
Cole Langner <clangner@iastate.edu>
2021-10-20 19:47:40 -0500

    add organization

---
+++ Backend/scribbleshare/src/main/java/edu/iastate/scribbleshare/Post/Post.java
@@ -0,0 +1,70 @@
+package edu.iastate.scribbleshare.Post;
+
+import java.sql.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Post {
+    
+@Id
+private int ID;
+
+private String username;
+
+private Date datePosted;
+
+private String path;
+
+private int likeCount;
+
+private int commentCount;
+
+
+
+public int getID()
+{
+    return this.ID;
+}
+
+public String getUsername(){
+    return this.username;
+}
+
+public void setUsername(String username){
+    this.username = username;
+}
+
+public Date getDatePosted(){
+    return this.datePosted;
+}
+
+public void setDatePosted(Date date){
+    this.datePosted = date;
+}
+
+public String getPath(){
+    return this.path;
+}
+
+public void setPath(String path){
+    this.path = path;
+}
+
+public int getLikeCount(){
+    return this.likeCount;
+}
+
+public void setLikeCount(int likes){
+    this.likeCount = likes;
+}
+
+public int getCommentCount(){
+    return this.commentCount;
+}
+
+public void setCommentCount(int comment){
+    this.commentCount = comment;
+}
+}
