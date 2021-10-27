2b7f24e7029a8c6dda7b29932bf2c3db450ee51e
Corbeno <corbin@kemsfamily.us>
2021-10-27 13:23:12 -0500

    add more. Not done yet

--- Frontend/app/src/main/java/com/example/scribbleshare/homepage/AdapterPosts.java
+++ Frontend/app/src/main/java/com/example/scribbleshare/homepage/AdapterPosts.java
@@ -1,26 +1,44 @@
 package com.example.scribbleshare.homepage;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.scribbleshare.R;
 
 import java.util.List;
 
 public class AdapterPosts extends RecyclerView.Adapter<AdapterPosts.Holder>{
 
     List<PostModel> postModels;
+    Context context;
+
+    public AdapterPosts(Context context, List<PostModel> postModels) {
+        this.context = context;
+        this.postModels = postModels;
+    }
 
     @NonNull
     @Override
     public Holder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View view = LayoutInflater.from(context).inflate(R.layout.post_individual, parent, false);
+        return new Holder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull Holder holder, int position) {
+        String profileName = postModels.get(position).getProfileName();
 
+        holder.profileName.setText(profileName);
+
+        //TODO set holder.thing.setOnClickListener here
     }
 
     @Override
@@ -29,8 +47,20 @@ public class AdapterPosts extends RecyclerView.Adapter<AdapterPosts.Holder>{
     }
 
     class Holder extends RecyclerView.ViewHolder {
+        ImageView scribble;
+        TextView profileName, likeCount, commentCount;
+        ImageButton likeButton, commentButton, shareButton;
+        //TODO add other buttons/textviews/etc
+
         public Holder(@NonNull View itemView) {
             super(itemView);
+            scribble = itemView.findViewById(R.id.post);
+            profileName = itemView.findViewById(R.id.profile_name);
+            likeCount = itemView.findViewById(R.id.post_like_count);
+            commentCount = itemView.findViewById(R.id.post_comment_count);
+            likeButton = itemView.findViewById(R.id.post_like_button);
+            commentButton = itemView.findViewById(R.id.post_comment_button);
+            shareButton = itemView.findViewById(R.id.post_share_button);
         }
     }
 }
--- Frontend/app/src/main/java/com/example/scribbleshare/homepage/PostModel.java
+++ Frontend/app/src/main/java/com/example/scribbleshare/homepage/PostModel.java
@@ -3,6 +3,9 @@ package com.example.scribbleshare.homepage;
 public class PostModel {
 
     String id;
+    String profileName;
+    int likeCount;
+    int commentCount;
 
     public PostModel() {}
 
@@ -16,6 +19,30 @@ public class PostModel {
 
     public void setId(String id){
         this.id = id;
+    }
+
+    public String getProfileName(){
+        return profileName;
+    }
+
+    public void setProfileName(String profileName){
+        this.profileName = profileName;
+    }
+
+    public int getLikeCount(){
+        return likeCount;
+    }
+
+    public void setLikeCount(int likeCount){
+        this.likeCount = likeCount;
+    }
+
+    public int getCommentCount(){
+        return commentCount;
+    }
+
+    public void setCommentCount(int commentCount){
+        this.commentCount = commentCount;
     }
 
 }
