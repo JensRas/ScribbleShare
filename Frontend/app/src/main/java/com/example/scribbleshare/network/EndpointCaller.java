7b00e22d91f8f01d42c8414ed7e4183e9af82a88
Cole Langner <clangner@iastate.edu>
2021-11-01 19:04:54 -0500

    fix

--- Frontend/app/src/main/java/com/example/scribbleshare/network/EndpointCaller.java
+++ Frontend/app/src/main/java/com/example/scribbleshare/network/EndpointCaller.java
@@ -24,8 +24,8 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class EndpointCaller<T> {
-    public static final String baseURL = "http://coms-309-010.cs.iastate.edu:8080";
-    //public static final String baseURL = "http://10.0.2.2:8080";
+    //public static final String baseURL = "http://coms-309-010.cs.iastate.edu:8080";
+    public static final String baseURL = "http://10.0.2.2:8080";
 
 
     private final Context context;
