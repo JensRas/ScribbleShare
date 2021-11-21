a8fd30652469a350e39f4fb0f6c9308ef25afe86
Corbeno <corbin@kemsfamily.us>
2021-11-21 00:33:24 -0600

    fix some errors between front and back end

--- Backend/scribbleshare/src/main/java/edu/iastate/scribbleshare/User/UserController.java
+++ Backend/scribbleshare/src/main/java/edu/iastate/scribbleshare/User/UserController.java
@@ -28,16 +28,17 @@ public class UserController {
 
     @ApiOperation(value = "Add New User", response = String.class, tags= "Users")
     @PutMapping(path="/users/new")
-    public @ResponseBody String addNewUser(@RequestParam String username, @RequestParam String password){
-        //TODO add checking if username is allowed
-
+    public @ResponseBody User addNewUser(HttpServletResponse response, @RequestParam String username, @RequestParam String password){
         if(userRepository.findById(username).isPresent()){
           //handle invalid username
-          return "username already exists";
+          Status.formResponse(response, HttpStatus.BAD_REQUEST, username + " already exists"); 
+          return null;
         }
-        userRepository.save(new User(username, password));
-        return "new user created";
+        User user = new User(username, password);
+        userRepository.save(user);
+        return user;
     }
+
     @ApiOperation(value = "Get All Users", response = Iterable.class, tags = "Users")
     @GetMapping(path="/users")
     public @ResponseBody Iterable<User> getAllUsers() {
@@ -57,6 +58,7 @@ public class UserController {
     @ApiOperation(value = "Log in User", response = User.class, tags= "Users")
     @GetMapping(path="/users/login")
     public @ResponseBody User login(HttpServletResponse response, @RequestParam String username, @RequestParam String password){
+      logger.info("login request");
       Optional<User> optionalUser = userRepository.findById(username);
       if(!optionalUser.isPresent()){
         Status.formResponse(response, HttpStatus.NOT_FOUND, username + " not found"); 
