d769e0040029b029e861f32f5161b55c75a6ea13
JensRas <jensr@iastate.edu>
2021-11-30 11:51:42 -0600

    Fixed buttons

--- Frontend/app/src/main/java/com/example/scribbleshare/profilepage/ProfilePage.java
+++ Frontend/app/src/main/java/com/example/scribbleshare/profilepage/ProfilePage.java
@@ -1,12 +1,19 @@
 package com.example.scribbleshare.profilepage;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.scribbleshare.MySingleton;
 import com.example.scribbleshare.R;
+import com.example.scribbleshare.activitypage.ActivityPage;
+import com.example.scribbleshare.drawingpage.DrawingPage;
+import com.example.scribbleshare.homepage.HomePage;
+import com.example.scribbleshare.searchpage.SearchPage;
 
 /**
  * TODO implement
@@ -21,5 +28,49 @@ public class ProfilePage extends AppCompatActivity implements ProfilePageView {
         ProfilePagePresenter presenter = new ProfilePagePresenter(this, getApplicationContext());
         String username = MySingleton.getInstance(this).getApplicationUser().getUsername();
         presenter.getFollowers(username); //when the request is done it calls "setHomePagePosts below
+
+        // Icon buttons
+        ImageButton home_button = (ImageButton) findViewById(R.id.btn_home);
+        home_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(view.getContext(), HomePage.class));
+            }
+        });
+
+        ImageButton search_button = (ImageButton) findViewById(R.id.btn_search);
+        search_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(view.getContext(), SearchPage.class));
+            }
+        });
+
+        ImageButton create_new_button = (ImageButton) findViewById(R.id.btn_create_new);
+        create_new_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(view.getContext(), DrawingPage.class);
+                intent.putExtra("drawContext", "newPost");
+                startActivity(intent);
+            }
+        });
+
+        ImageButton activity_button = (ImageButton) findViewById(R.id.btn_activity);
+        activity_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(view.getContext(), ActivityPage.class));
+            }
+        });
+
+        ImageButton profile_button = (ImageButton) findViewById(R.id.btn_profile);
+        profile_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Already on profile page
+                //startActivity(new Intent(view.getContext(), ProfilePage.class));
+            }
+        });
     }
 }
