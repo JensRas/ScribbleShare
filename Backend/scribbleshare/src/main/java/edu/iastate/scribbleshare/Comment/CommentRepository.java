e1e29190e10d510a5ea223393267b4ebece40e92
Corbeno <corbin@kemsfamily.us>
2021-12-06 22:35:03 -0600

    add missing repository query

--- Backend/scribbleshare/src/main/java/edu/iastate/scribbleshare/Comment/CommentRepository.java
+++ Backend/scribbleshare/src/main/java/edu/iastate/scribbleshare/Comment/CommentRepository.java
@@ -1,7 +1,12 @@
 package edu.iastate.scribbleshare.Comment;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 public interface CommentRepository extends CrudRepository<Comment, Integer>{
+
+    @Query(value = "SELECT count(*) FROM liked_comments lc where lc.comment_id=:comment_id AND lc.username=:username", nativeQuery = true)
+    public int getCommentLikedByUser(@Param("comment_id") int comment_id, @Param("username") String username);
     
 }
